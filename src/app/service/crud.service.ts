2bc5846870e924a3248c1e20dde85914a93b536f
Chemi <116808913+Chemi90@users.noreply.github.com>
2024-07-21 19:57:46 +0200

    change back php to python

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -5,6 +5,43 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CrudService {
+  public server = 'https://josemiguelruizguevara.com:5000/'; // Nueva URL para Flask
+
+  constructor(private http: HttpClient) {}
+
+  create(endpoint: string, data: any) {
+    return this.http.post(this.server + endpoint, data); // Elimina '.POST.php'
+  }
+
+  read(endpoint: string, id: any = null) {
+    return this.http.get(
+      this.server + endpoint + (id ? '?id=' + id : '') // Elimina '.GET.php'
+    );
+  }
+
+  update(endpoint: string, id: number, data: any) {
+    return this.http.put(
+      this.server + endpoint + '?id=' + id, // Elimina '.PUT.php'
+      data
+    );
+  }
+
+  delete(endpoint: string, id: number) {
+    return this.http.delete(
+      this.server + endpoint + '?id=' + id // Elimina '.DELETE.php'
+    );
+  }
+}
+
+
+/*
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CrudService {
   public server = 'https://josemiguelruizguevara.com/ia/API/';
 
   constructor(private http: HttpClient) {}
@@ -32,3 +69,4 @@ export class CrudService {
     );
   }
 }
+*/
